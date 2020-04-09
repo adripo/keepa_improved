c0a19fdcfa4cfb759d735f2406e65e823d63aad1
adripo <adrian.polimi@gmail.com>
2020-04-09 19:29:05 +0000

    Updated to v0.3.2
    included modifications made offline

--- KeepaImproved.meta.js
+++ KeepaImproved.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Keepa Improved
 // @namespace    https://github.com/adripo
-// @version      0.3
+// @version      0.3.2
 // @author       adripo
 // @description  Improve keepa deals listing
 // @homepage     https://github.com/adripo/keepa_improved
@@ -11,9 +11,12 @@
 // @supportURL   https://github.com/adripo/keepa_improved/wiki
 // @match        https://keepa.com/*
 // @require      https://code.jquery.com/jquery-1.12.4.min.js
+// @connect      keepa.com
+// @connect      amazon.com
 // @run-at       document-start
 // @grant        GM_xmlHttpRequest
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_listValues
 // @grant        GM_addStyle
 // ==/UserScript==
