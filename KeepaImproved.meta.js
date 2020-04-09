d77f17c50e553c14de680da3db3653ea9ff501d0
adripo <adrian.polimi@gmail.com>
2020-04-09 19:11:28 +0000

    Updated to v0.3.1

---
+++ KeepaImproved.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Keepa Improved
+// @namespace    https://github.com/adripo
+// @version      0.3.1
+// @author       adripo
+// @description  Improve keepa deals listing
+// @homepage     https://github.com/adripo/keepa_improved
+// @icon         https://keepa.com/favicon.ico
+// @updateURL    https://raw.githubusercontent.com/adripo/keepa_improved/master/KeepaImproved.meta.js
+// @downloadURL  https://raw.githubusercontent.com/adripo/keepa_improved/master/KeepaImproved.user.js
+// @supportURL   https://github.com/adripo/keepa_improved/wiki
+// @match        https://keepa.com/*
+// @require      https://code.jquery.com/jquery-1.12.4.min.js
+// @run-at       document-start
+// @grant        GM_xmlHttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// ==/UserScript==
